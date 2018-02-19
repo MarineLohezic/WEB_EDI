bd85b3fa2e16d4518e773387b404688f28701968
MarineLohezic <lohezic.marine@gmail.com>
2018-02-19 08:44:53 +0100

    fixation css index.php et inscription.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
         <title>WEB_EDI</title>
     </head>
     <body>
-    	<header> <h1>WEB_EDI</h1> </header>
+    	<header><div class="center"><h1>WEB_EDI</h1></div></header>
         <div class="error">
         <?php
         	if (isset($_GET['valide'])) // On a eu une erreur
@@ -17,7 +17,6 @@
 			{
 				echo 'Veuillez valider votre inscription avant de vous connecter' ;
 			}
-
         	if (isset($_GET['auth'])) // On a eu une erreur
 			{
 				echo 'Veuillez vous connecter pour continuer' ;
@@ -33,21 +32,27 @@
 		?>
 		</div> 
 		<form action="inscription.php" method="post">
-			<input class="bold" type="submit" value="Inscription"/>
-		</form>
-		<h4> Se connecter : </h4>
-		<form action="do.authentification.php" method="post">
-			<div class="colonne">
-				<label for="identifiant"> Identifiant : </label>
-
-				<input id="identifiant" type="text" name="login" required value= <?php if(isset($_COOKIE["login"])){ 
-						echo $_COOKIE["login"];}else{echo "";} ?>>
-				<label for="mdp"> Mot de passe : </label>
-				<input id="mdp" type="password" name="password" required />
-				<div class="sub">
-			 		<input type="submit" value="Valider">
-			 	</div>
+			<div class="center">
+				<input class="bold" type="submit" value="---> Inscription <---"/>
 			</div>
 		</form>
+		<div class="center">
+			<h4> Se connecter : </h4>
+		</div>
+		<div class="center">
+			<form action="do.authentification.php" method="post">
+				<div class="colonne">
+					<label for="identifiant"> Identifiant : </label>
+
+					<input id="identifiant" type="text" name="login" required value= <?php if(isset($_COOKIE["login"])){ 
+							echo $_COOKIE["login"];}else{echo "";} ?>>
+					<label for="mdp"> Mot de passe : </label>
+					<input id="mdp" type="password" name="password" required />
+					<div class="sub">
+				 		<input type="submit" value="Valider">
+				 	</div>
+				</div>
+			</form>
+		</div>
     </body>
 </html>
--- inscription.php
+++ inscription.php
@@ -6,11 +6,13 @@
         <title>WEB_EDI</title>
     </head>
     <body>
-    	<header> <h1>WEB_EDI</h1> </header>
+    	<header><div class="center"><h1>WEB_EDI</h1></div></header>
     	<form action="index.php" method="post">
-			<input class="bold" type="submit" value="Se connecter">
+    		<div class="center">
+				<input class="bold" type="submit" value="Se connecter">
+			</div>
 		</form>
-    	<h4>Inscription :</h4>
+
     	<div class="error">
         <?php
 			if (isset($_GET['error_password'])) 
@@ -31,22 +33,29 @@
 			}
 		?>
 		</div> 
-    	<form action="do.inscription.php" method="post">
-		<div class="colonne">
-			<label for="identifiant"> Identifiant : </label>
-			<input id="identifiant" type="email" name="login" required/>
-			<label for="mdp1"> Mot de passe : </label>
-		 	<input id="mdp1" type="password" name="password1" required/>
-		 	<label for="mdp2"> Confirmation : </label>
-		 	<input id="mdp2" type="password" name="password2" required/>
-		 	<label for="nom"> Nom : </label>
-			<input id="nom" type="text" name="lastname" required />
-			<label for="prenom"> Prénom : </label>
-			<input id="prenom" type="text" name="firstname" required/>
-			<div class="sub">
-				<input type="submit" value="S'inscrire">
-			</div>
+
+		<div class="center">
+    		<h4>Inscription :</h4>
+    	</div>
+
+    	<div class="center">
+	    	<form action="do.inscription.php" method="post">
+				<div class="colonne">
+					<label for="identifiant"> Identifiant : </label>
+					<input id="identifiant" type="email" name="login" required/>
+					<label for="mdp1"> Mot de passe : </label>
+				 	<input id="mdp1" type="password" name="password1" required/>
+				 	<label for="mdp2"> Confirmation : </label>
+				 	<input id="mdp2" type="password" name="password2" required/>
+				 	<label for="nom"> Nom : </label>
+					<input id="nom" type="text" name="lastname" required />
+					<label for="prenom"> Prénom : </label>
+					<input id="prenom" type="text" name="firstname" required/>
+					<div class="sub">
+						<input type="submit" value="S'inscrire">
+					</div>
+				</div>
+			</form>
 		</div>
-		</form>
     </body>
 </html>
